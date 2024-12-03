aa7a842fb65610cd8a6ae03334e52464d7226778
Polyshinnel <polyshinnel@gmail.com>
2024-12-03 14:23:16 +0300

    fix cart prices

--- resources/views/Layers/HomeLayer.blade.php
+++ resources/views/Layers/HomeLayer.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/js/splide/css/splide-core.min.css">
     <link rel="stylesheet" href="/assets/js/fancybox/fancybox.css">
-    <link rel="stylesheet" href="/assets/css/style.css?ver=78541">
+    <link rel="stylesheet" href="/assets/css/style.css?ver=123512">
     <title>@yield('page_title')</title>
 </head>
 <body>
